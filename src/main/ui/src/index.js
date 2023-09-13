0a069a46900a833164deee0f80c20810e9aad8c3
Lichtner Levente <lichtner.levente@gmail.com>
2023-09-13 13:34:58 +0200

    start pages

--- src/main/ui/src/index.js
+++ src/main/ui/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+export default class Layout {
+}
+import Layout from ""
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
